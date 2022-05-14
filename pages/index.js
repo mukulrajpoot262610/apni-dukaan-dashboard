e8856f9a6d99e807b19a363600d4516f1d3f9e79
mukulrajpoot262610 <mukulrajpoot262610@gmail.com>
2022-05-14 11:47:05 +0530

    ADD: order

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import Head from "next/head";
 import Image from 'next/image'
+import Link from 'next/link'
+import Footer from "../components/Layout/Footer";
 
 export default function Home() {
   return (
@@ -11,27 +13,31 @@ export default function Home() {
       <div className="fixed bg-white z-50 h-20 w-full border-b flex justify-around items-center">
         <div className="flex items-center">
           <Image src='/logo.svg' height={50} width={50} alt="" objectFit="contain" />
-          <h1 className="font-bold mt-3 text-xl">Apni Dukaan</h1>
+          <h1 className="font-bold mt-3 text-xl hidden lg:block">Apni Dukaan</h1>
         </div>
-        <button className="btn btn-wide">Start For Free</button>
+        <Link href="/login" passHref>
+          <button className="btn lg:btn-wide">Start For Free</button>
+        </Link>
       </div>
 
-      <div className="w-9/12 mx-auto flex min-h-screen justify-center items-center">
-        <div className="w-1/2 flex flex-col justify-center items-start p-6">
-          <h1 className="text-6xl font-bold max-w-lg">Start
+      <div className="w-full lg:w-9/12 mx-auto flex min-h-screen justify-center items-center flex-col lg:flex-row">
+        <div className="w-full lg:w-1/2 flex flex-col justify-center items-start p-6">
+          <h1 className="text-5xl font-bold max-w-lg">Start
             selling
             online.</h1>
           <p className="my-4 max-w-lg">Dukaan is the easiest way to start, run and grow your online business.</p>
-          <button className="btn btn-wide">Start For Free</button>
+          <Link href="/login" passHref>
+            <button className="btn btn-wide">Start For Free</button>
+          </Link>
         </div>
-        <div className="w-1/2"></div>
+        <div className="w-full lg:w-1/2"></div>
       </div>
 
       <div className="bg-gray-100 flex justify-center items-center">
-        <div className="w-9/12 my-24">
+        <div className="w-full lg:w-9/12 my-24 p-6">
           <h1 className="text-3xl font-bold">The easiest way to sell anything online.</h1>
           <p className="text-base my-2">Start your brand new eCommerce store and impress your customers, with Apni Dukaan!</p>
-          <div className="mt-8 flex gap-4">
+          <div className="mt-8 flex lg:flex-row flex-col gap-4">
             <div className="bg-white shadow-md rounded-lg p-3">
               <img src={"https://mydukaan.io/_next/static/images/food-631d92f5be77ef7b69d62ebffd6a1ffc.png"} height={100} width={300} className="rounded-lg" />
               <h1 className="mt-2 font-bold px-2">Start Selling Food</h1>
@@ -49,13 +55,13 @@ export default function Home() {
       </div>
 
       <div className="flex justify-center items-center">
-        <div className="min-h-screen w-8/12 my-24">
+        <div className="min-h-screen w-full lg:w-8/12 my-24 p-6">
           <h1 className="text-3xl font-bold">Your dream business just three steps away!</h1>
-          <div className="flex mt-16 items-center justify-between gap-10">
-            <div className="w-1/2">
+          <div className="flex lg:flex-row flex-col mt-16 items-center justify-between gap-10">
+            <div className="w-full lg:w-1/2">
               <img src="https://mydukaan.io/images/create-store-new.svg" width={400} />
             </div>
-            <div className="w-1/2">
+            <div className="w-full lg:w-1/2">
               <h3 className="font-semibold">STEP 1 </h3>
               <h2 className="font-bold mb-4">Name your store</h2>
               <p>The nights you’ve lost, the fights you’ve fought... to find the perfect name
@@ -63,23 +69,23 @@ export default function Home() {
                 <br />Name your store, choose a category and say a prayer.</p>
             </div>
           </div>
-          <div className="flex mt-16 items-center justify-between gap-10">
-            <div className="w-1/2">
+          <div className="flex lg:flex-row flex-col-reverse mt-16 items-center justify-between gap-10">
+            <div className="w-full lg:w-1/2">
               <h3 className="font-semibold">STEP 2 </h3>
               <h2 className="font-bold mb-4">Add your products</h2>
               <p>Bakery or boutique, art or apparel...now it’s time to sell.
                 <br /> Let the world know what you have to offer.
                 <br />Add your products, set the price and wish you good luck!</p>
             </div>
-            <div className="w-1/2 flex justify-end items-center">
+            <div className="w-full lg:w-1/2">
               <img src="https://mydukaan.io/images/product-listing.svg" width={400} />
             </div>
           </div>
-          <div className="flex mt-16 items-center justify-between gap-10">
-            <div className="w-1/2">
+          <div className="flex lg:flex-row flex-col mt-16 items-center justify-between gap-10">
+            <div className="w-full lg:w-1/2">
               <img src="https://mydukaan.io/images/start-selling.svg" width={400} />
             </div>
-            <div className="w-1/2">
+            <div className="w-full lg:w-1/2">
               <h3 className="font-semibold">STEP 3 </h3>
               <h2 className="font-bold mb-4">Start selling</h2>
               <p>The moment of truth. The leap of faith.
@@ -88,6 +94,10 @@ export default function Home() {
             </div>
           </div>
         </div>
+      </div>
+
+      <div className="w-9/12 mx-auto">
+        <Footer />
       </div>
     </div>
   )
